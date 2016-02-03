c513c7f759a22d30161efaf756f08ec940b2c294
Baldwin Browne <Rooke83@users.noreply.github.com>
2016-02-02 17:51:07 -0800

    made additions to the __init__.py file, added templates, and bootstrap.css file

--- FlaskApp/FlaskApp/__init__.py
+++ FlaskApp/FlaskApp/__init__.py
@@ -1,11 +1,33 @@
-from flask import Flask
+from flask import *
+from functools import wraps
 
 app = Flask(__name__)
 
+app.secret_key = "adsasc"
+
 @app.route('/')
 def homepage():
-    return "Hello, world!"
+    return render_template('home.html')
 
+@app.route('/upload')
+def upload():
+    return render_template('upload.html')
+    
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    return redirect(url_for('homepage'))
+    
+@app.route('/log', methods=["GET", "POST"])
+def log():
+    error = None
+    if request.method == "POST":
+        if request.form['username'] != "b_browne" or request.form['password'] != "admin":
+            error = "Access not permitted"
+        else:
+            session['logged_in'] = True
+            return redirect url_for('upload')
+    return render_template('log.html', error = error)
 
 if __name__ == "__main__":
     app.run()
